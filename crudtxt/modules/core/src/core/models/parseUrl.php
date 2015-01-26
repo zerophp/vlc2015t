f53fedec48833853ac1a939ef3afd3aa5df6de89
Agustin Calderon <agustincl@gmail.com>
2015-01-26 17:11:44 +0100

    Seudocode parseurl

--- crudtxt/modules/core/src/core/models/parseUrl.php
+++ crudtxt/modules/core/src/core/models/parseUrl.php
@@ -8,7 +8,24 @@ function parseUrl()
     
     // Mientras que ultimo elemento vacio, eliminarlo
     // Si longitud superior a 3 y par error 412
+    // De lo contrario hacer array de params
     
+    // If file_exist controller && controller not ''
+        // OK 
+            // If action in array && not ''
+                // Ok 
+                    // Return request
+                // KO
+                    // Return error 404
+            // else action ''
+                // action = index
+                
+                    
+        // KO
+            // Return erro 404
+   
+    // else controller ''
+        // Return controller = index, action =index
     
     
     echo $_SERVER['REQUEST_URI'];
